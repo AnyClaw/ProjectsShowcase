1b1a606f56f8843cafd0b5ea22a6ddfe0ee01aac
AnyClaw <l.kolryser@yandex.ru>
2025-03-21 12:55:30 +0300

    first commit

---
+++ src/main/java/com/example/ProjectsShowcase/ProjectsShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectsShowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsShowcaseApplication.class, args);
+	}
+
+}
